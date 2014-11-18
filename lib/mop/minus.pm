492982d6d811344ffa30825fcac71fe6ec92bd01
Yuki Kimoto <kimoto.yuki@gmail.com>
2014-11-18 21:43:08 +0900

    cleanup

--- lib/mop/minus.pm
+++ lib/mop/minus.pm
@@ -1,310 +1,306 @@
 use v5.20;
 
-package mop::minus;
-
-our $VERSION = '0.01';
-
 use strict;
 use warnings;
 use feature ();
-
-use Carp 'croak';
 use mop::minus::object;
 
-use Parse::Keyword {
-  extends => \&extends_parser,
-  with => \&with_parser,
-  has => \&has_parser
-};
+package mop::minus 0.01 {
 
-sub import {
-  my $class = shift;
-  
-  # Caller package
-  my $caller = caller;
-  
-  # Subroutine signatures
-  feature->import('signatures');
-  warnings->unimport('experimental::signatures');
-  
-  # Inherit base class
-  no strict 'refs';
-  @{"${caller}::ISA"} = ('mop::minus::object');
-  
-  # Register keywords
-  no warnings 'redefine';
-  *{"${caller}::has"} = \&has;
-  *{"${caller}::extends"} = \&extends;
-  *{"${caller}::with"} = \&with;
-}
-
-sub create_accessor {
-  my ($class, $attr, $opt) = @_;
-  
-  # Check attribute name
-  Carp::croak qq{Attribute "$attr" invalid} unless $attr =~ /^[a-zA-Z_]\w*$/;
-  
-  # Default
-  my $default = $opt->{default};
-  
-  # Header (check arguments)
-  my $code = "*{\"${class}::$attr\"} = sub {\n  if (\@_ == 1) {\n";
-
-  # No default value (return value)
-  unless (exists $opt->{default}) { $code .= "    return \$_[0]{'$attr'};" }
-
-  # Default value
-  else {
-
-    Carp::croak "Default has to be a code reference or constant value (${class}::$attr)"
-      if ref $default && ref $default ne 'CODE';
-
-    # Return value
-    $code .= "    return \$_[0]{'$attr'} if exists \$_[0]{'$attr'};\n";
-
-    # Return default value
-    $code .= "    return \$_[0]{'$attr'} = ";
-    $code .= ref $default eq 'CODE' ? '$default->($_[0]);' : '$default;';
-  }
-
-  # Store value
-  $code .= "\n  }\n  \$_[0]{'$attr'} = \$_[1];\n";
-
-  # Footer (return invocant)
-  $code .= "  \$_[0];\n}";
-
-  # We compile custom attribute code for speed
-  no strict 'refs';
-  warn "-- Attribute $attr in $class\n$code\n\n" if $ENV{MOP_MINUS__DEBUG};
-  Carp::croak "mop::minus error: $@" unless eval "$code;1";
-}
-
-sub has {
-  my ($class, $name, $default_exists, $default_code) = @_;
-  
-  if ($default_exists) {
-    create_accessor($class, $name, {default => $default_code->()});
-  }
-  else {
-    create_accessor($class, $name);
-  }
-}
-
-sub has_parser {
-  my $caller = compiling_package;
-  
-  lex_read_space;
-
-  my $name = parse_name('attribute');
-
-  lex_read_space;
-  
-  my $default_exists;
-  my $default;
-  if (lex_peek eq '=') {
-    $default_exists = 1;
-    lex_read;
-    lex_read_space;
-    $default = parse_fullexpr;
-  }
-
-  lex_read_space;
-
-  die "Couldn't parse attribute $name" unless lex_peek eq ';';
-  lex_read;
-
-  return (sub { ($caller, $name, $default_exists, $default) }, 1);
-}
-
-sub extends {
-  my ($class_info) = @_;
-  
-  # Class infomation
-  my $class = $class_info->{class};
-  my $base_class = $class_info->{extends};
-  
-  # Extends
-  my $base_class_path = $base_class;
-  $base_class_path =~ s/::|'/\//g;
-  require "$base_class_path.pm";
-  no strict 'refs';
-  @{"${class }::ISA"} = ($base_class);
-  
-  return 1;
-}
-
-sub extends_parser {
-
-  my $caller = compiling_package;
-
-  lex_read_space;
-  
-  my @extends;
-  if (@extends = parse_multiple_values('extends')) {
-    if (@extends > 1) {
-      croak "Can't extends multiple classes(" . join(',', @extends) . ")";
-    }
-  }
-  
-  lex_read_space;
-  
-  my $class_info = {
-    class      => $caller,
-    extends   => $extends[0],
+  use Carp 'croak';
+  use Parse::Keyword {
+    extends => \&extends_parser,
+    with => \&with_parser,
+    has => \&has_parser
   };
 
-  return (sub { $class_info }, 1);
-}
+  sub import {
+    my $class = shift;
+    
+    # Caller package
+    my $caller = caller;
+    
+    # Subroutine signatures
+    feature->import('signatures');
+    warnings->unimport('experimental::signatures');
+    
+    # Inherit base class
+    no strict 'refs';
+    @{"${caller}::ISA"} = ('mop::minus::object');
+    
+    # Register keywords
+    no warnings 'redefine';
+    *{"${caller}::has"} = \&has;
+    *{"${caller}::extends"} = \&extends;
+    *{"${caller}::with"} = \&with;
+  }
 
-my $role_id = 1;
+  sub create_accessor {
+    my ($class, $attr, $opt) = @_;
+    
+    # Check attribute name
+    croak qq{Attribute "$attr" invalid} unless $attr =~ /^[a-zA-Z_]\w*$/;
+    
+    # Default
+    my $default = $opt->{default};
+    
+    # Header (check arguments)
+    my $code = "*{\"${class}::$attr\"} = sub {\n  if (\@_ == 1) {\n";
 
-sub with {
-  my ($class_info) = @_;
-  
-  my $class = $class_info->{class};
-  my $roles = $class_info->{with};
+    # No default value (return value)
+    unless (exists $opt->{default}) { $code .= "    return \$_[0]{'$attr'};" }
 
-  # Roles
-  for my $role (@$roles) {
+    # Default value
+    else {
+
+      croak "Default has to be a code reference or constant value (${class}::$attr)"
+        if ref $default && ref $default ne 'CODE';
+
+      # Return value
+      $code .= "    return \$_[0]{'$attr'} if exists \$_[0]{'$attr'};\n";
+
+      # Return default value
+      $code .= "    return \$_[0]{'$attr'} = ";
+      $code .= ref $default eq 'CODE' ? '$default->($_[0]);' : '$default;';
+    }
+
+    # Store value
+    $code .= "\n  }\n  \$_[0]{'$attr'} = \$_[1];\n";
+
+    # Footer (return invocant)
+    $code .= "  \$_[0];\n}";
+
+    # We compile custom attribute code for speed
+    no strict 'refs';
+    warn "-- Attribute $attr in $class\n$code\n\n" if $ENV{MOP_MINUS__DEBUG};
+    croak "mop::minus error: $@" unless eval "$code;1";
+  }
+
+  sub has {
+    my ($class, $name, $default_exists, $default_code) = @_;
     
-    my $role_file = $role;
-    $role_file =~ s/::/\//g;
-    $role_file .= ".pm";
-    require $role_file;
-    
-    my $role_path = $INC{$role_file};
-    open my $fh, '<', $role_path
-      or Carp::croak "Can't open file $role_path: $!";
-    
-    my $role_content = do { local $/; <$fh> };
-    my $role_for_file = "mop::minus::role::id${role_id}::$role";
-    $role_id++;
-    $INC{$role_for_file} = undef;
-    
-    my $role_for = $role_for_file;
-    $role_for =~ s/\//::/g;
-    $role_for =~ s/\.pm$//;
-    
-    my $role_for_content = $role_content;
-    if ($role_for_content =~ s/package\s+([a-zA-Z0-9:]+)/package $role_for/) {
-      eval $role_for_content;
-      Carp::croak $@ if $@;
+    if ($default_exists) {
+      create_accessor($class, $name, {default => $default_code->()});
     }
     else {
-      Carp::croak "Can't clone $role";
-    }
-    
-    {
-      no strict 'refs';
-      my $parent = ${"${class}::ISA"}[0];
-      @{"${class}::ISA"} = ($role_for);
-      if ($parent) {
-        @{"${role_for}::ISA"} = ($parent);
-      }
+      create_accessor($class, $name);
     }
   }
-  
-  return 1;
-}
 
-sub with_parser {
-  
-  my $caller = compiling_package;
+  sub has_parser {
+    my $caller = compiling_package;
+    
+    lex_read_space;
 
-  lex_read_space;
-  
-  my @with = parse_multiple_values('with');
-  
-  my $class_info = {
-    class      => $caller,
-    with      => \@with,
-  };
+    my $name = parse_name('attribute');
 
-  return (sub { $class_info }, 1);
-}
-
-sub parse_name {
-  my ($what, $allow_package) = @_;
-  my $name = '';
-
-  # XXX this isn't quite right, i think, but probably close enough for now?
-  my $start_rx = qr/^[\p{ID_Start}_]$/;
-  my $cont_rx  = qr/^\p{ID_Continue}$/;
-
-  my $char_rx = $start_rx;
-
-  while (1) {
-    my $char = lex_peek;
-    last unless length $char;
-    if ($char =~ $char_rx) {
-      $name .= $char;
+    lex_read_space;
+    
+    my $default_exists;
+    my $default;
+    if (lex_peek eq '=') {
+      $default_exists = 1;
       lex_read;
-      $char_rx = $cont_rx;
+      lex_read_space;
+      $default = parse_fullexpr;
     }
-    elsif ($allow_package && $char eq ':') {
-      if (lex_peek(3) !~ /^::(?:[^:]|$)/) {
-        my $invalid = $name . read_tokenish();
-        die "Invalid identifier: $invalid";
+
+    lex_read_space;
+
+    die "Couldn't parse attribute $name" unless lex_peek eq ';';
+    lex_read;
+
+    return (sub { ($caller, $name, $default_exists, $default) }, 1);
+  }
+
+  sub extends {
+    my ($class_info) = @_;
+    
+    # Class infomation
+    my $class = $class_info->{class};
+    my $base_class = $class_info->{extends};
+    
+    # Extends
+    my $base_class_path = $base_class;
+    $base_class_path =~ s/::|'/\//g;
+    require "$base_class_path.pm";
+    no strict 'refs';
+    @{"${class }::ISA"} = ($base_class);
+    
+    return 1;
+  }
+
+  sub extends_parser {
+    my $caller = compiling_package;
+
+    lex_read_space;
+    
+    my @extends;
+    if (@extends = parse_multiple_values('extends')) {
+      if (@extends > 1) {
+        croak "Can't extends multiple classes(" . join(',', @extends) . ")";
       }
-      $name .= '::';
-      lex_read(2);
     }
-    else {
-      last;
+    
+    lex_read_space;
+    
+    my $class_info = {
+      class      => $caller,
+      extends   => $extends[0],
+    };
+
+    return (sub { $class_info }, 1);
+  }
+
+  my $role_id = 1;
+
+  sub with {
+    my ($class_info) = @_;
+    
+    my $class = $class_info->{class};
+    my $roles = $class_info->{with};
+
+    # Roles
+    for my $role (@$roles) {
+      
+      my $role_file = $role;
+      $role_file =~ s/::/\//g;
+      $role_file .= ".pm";
+      require $role_file;
+      
+      my $role_path = $INC{$role_file};
+      open my $fh, '<', $role_path
+        or croak "Can't open file $role_path: $!";
+      
+      my $role_content = do { local $/; <$fh> };
+      my $role_for_file = "mop::minus::role::id${role_id}::$role";
+      $role_id++;
+      $INC{$role_for_file} = undef;
+      
+      my $role_for = $role_for_file;
+      $role_for =~ s/\//::/g;
+      $role_for =~ s/\.pm$//;
+      
+      my $role_for_content = $role_content;
+      if ($role_for_content =~ s/package\s+([a-zA-Z0-9:]+)/package $role_for/) {
+        eval $role_for_content;
+        croak $@ if $@;
+      }
+      else {
+        croak "Can't clone $role";
+      }
+      
+      {
+        no strict 'refs';
+        my $parent = ${"${class}::ISA"}[0];
+        @{"${class}::ISA"} = ($role_for);
+        if ($parent) {
+          @{"${role_for}::ISA"} = ($parent);
+        }
+      }
     }
+    
+    return 1;
   }
 
-  die read_tokenish() . " is not a valid $what name" unless length $name;
+  sub with_parser {
+    my $caller = compiling_package;
 
-  return $name;
-}
-
-sub read_tokenish {
-  my $token = '';
-  if ((my $next = lex_peek) =~ /[\$\@\%]/) {
-    $token .= $next;
-    lex_read;
-  }
-  while ((my $next = lex_peek) =~ /\S/) {
-    $token .= $next;
-    lex_read;
-    last if ($next . lex_peek) =~ /^\S\b/;
-  }
-  return $token;
-}
-
-sub parse_modifier_with_multiple_values {
-  my ($modifier) = @_;
-
-  my $modifier_length = length $modifier;
-
-  return unless lex_peek($modifier_length + 1) =~ /^$modifier\b/;
-
-  lex_read($modifier_length);
-  lex_read_space;
-
-  my @names;
-
-  do {
-    my $name = parse_name('role', 1);
-    push @names, $name;
     lex_read_space;
-  } while (lex_peek eq ',' && do { lex_read; lex_read_space; 1 });
+    
+    my @with = parse_multiple_values('with');
+    
+    my $class_info = {
+      class      => $caller,
+      with      => \@with,
+    };
 
-  return @names;
-}
+    return (sub { $class_info }, 1);
+  }
 
-sub parse_multiple_values {
+  sub parse_name {
+    my ($what, $allow_package) = @_;
+    my $name = '';
 
-  my @names;
+    # XXX this isn't quite right, i think, but probably close enough for now?
+    my $start_rx = qr/^[\p{ID_Start}_]$/;
+    my $cont_rx  = qr/^\p{ID_Continue}$/;
 
-  do {
-    my $name = parse_name('role', 1);
-    push @names, $name;
+    my $char_rx = $start_rx;
+
+    while (1) {
+      my $char = lex_peek;
+      last unless length $char;
+      if ($char =~ $char_rx) {
+        $name .= $char;
+        lex_read;
+        $char_rx = $cont_rx;
+      }
+      elsif ($allow_package && $char eq ':') {
+        if (lex_peek(3) !~ /^::(?:[^:]|$)/) {
+          my $invalid = $name . read_tokenish();
+          die "Invalid identifier: $invalid";
+        }
+        $name .= '::';
+        lex_read(2);
+      }
+      else {
+        last;
+      }
+    }
+
+    die read_tokenish() . " is not a valid $what name" unless length $name;
+
+    return $name;
+  }
+
+  sub read_tokenish {
+    my $token = '';
+    if ((my $next = lex_peek) =~ /[\$\@\%]/) {
+      $token .= $next;
+      lex_read;
+    }
+    while ((my $next = lex_peek) =~ /\S/) {
+      $token .= $next;
+      lex_read;
+      last if ($next . lex_peek) =~ /^\S\b/;
+    }
+    return $token;
+  }
+
+  sub parse_modifier_with_multiple_values {
+    my ($modifier) = @_;
+
+    my $modifier_length = length $modifier;
+
+    return unless lex_peek($modifier_length + 1) =~ /^$modifier\b/;
+
+    lex_read($modifier_length);
     lex_read_space;
-  } while (lex_peek eq ',' && do { lex_read; lex_read_space; 1 });
 
-  return @names;
+    my @names;
+
+    do {
+      my $name = parse_name('role', 1);
+      push @names, $name;
+      lex_read_space;
+    } while (lex_peek eq ',' && do { lex_read; lex_read_space; 1 });
+
+    return @names;
+  }
+
+  sub parse_multiple_values {
+
+    my @names;
+
+    do {
+      my $name = parse_name('role', 1);
+      push @names, $name;
+      lex_read_space;
+    } while (lex_peek eq ',' && do { lex_read; lex_read_space; 1 });
+
+    return @names;
+  }
 }
 
 1;
